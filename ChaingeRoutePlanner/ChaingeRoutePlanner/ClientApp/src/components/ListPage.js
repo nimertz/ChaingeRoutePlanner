943e886f742bdf13bb93eb6886c14aaff98bb670
Nikolaj Mertz <nikolajmertz@gmail.com>
2021-11-25 23:00:33 +0100

    progress on ListPage.js

--- ChaingeRoutePlanner/ChaingeRoutePlanner/ClientApp/src/components/ListPage.js
+++ ChaingeRoutePlanner/ChaingeRoutePlanner/ClientApp/src/components/ListPage.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup,useMapEvents  } from 'react-leaflet'
-import { Button, Row, Col, Container, Form } from 'reactstrap';
+import { Button, Row, Col, Container} from 'reactstrap';
 
 function LocationMarker(props) {
     const [position, setPosition] = useState(null)
@@ -40,129 +40,118 @@ function LocationMarker(props) {
 
 export class ListPage extends Component {
     static displayName = ListPage.name;
+
     constructor(props) {
         super(props);
         this.state = {
-            value: 'Howdy',
-            description: '',
-            pickup: false,
-            location: [0,0],
-            amount: '',
-            timeStart: '',
-            timeEnd: '',
-            timeSpan: '',
+            loading: true,
+            
             lat: 35.76218444303944,
             lng: 51.33657932281495,
-            checked: false
+            checked: false,
+            location: [0,0],
+            
+            vehicles: [],
+            shipments: [],
+            selectedShipments: [],
+            selectedVehicles: [],
         };
-    
-        this.handleSendData = this.handleSendData.bind(this);
-        this.handleCheckbox = this.handleCheckbox.bind(this);
     }
 
-        handleClick = (e) => {
-            console.log(e.latlng)
-        }
-
-        handleSendData(event) {
-
-            const jsonToSend = {
-                "Pickup": this.state.checked,
-                "Amount": this.state.amount,
-                "Location": [this.state.lat, this.state.lng],
-                "Time": [this.state.timeStart, this.state.timeEnd],
-                "TimeSpan": this.state.timeSpan
-            };
-
-            alert('Data send');
-            console.log('stuff', jsonToSend);
-            console.log(this.postPackage(this.state.checked, this.state.amount, this.state.lng, this.state.lat, this.state.description))
-
-            //event.preventDefault();
-        }
-
-        handleCheckbox(input){
-            this.setState({
-                checked: !this.state.checked
-              });
+    componentDidMount() {
+        this.populateLists();
     }
 
-    postPackage = async (pickup, amount, long, lat, descrip) => {
-        const location = window.location.hostname;
-        console.log('123', this.state.lon)
-        const settings = {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                // your expected POST request payload goes here
-                "pickup": pickup,
-                "description": descrip,
-                "amount": amount,
-                "location": [
-                    long,
-                    lat
-                ]
-            })
-
-        };
-        try {
-            const fetchResponse = await fetch(`Shipment`, settings);
-            const data = await fetchResponse.json();
-            return data;
-        } catch (e) {
-            return e;
-        }
-
+    static renderVehicleList(vehicles) {
+        return (
+            <ul className="list-group">
+                <h5>Vehicles</h5>
+                {vehicles.map(vehicle =>
+                    <li className="list-group-item">
+                        <input className="form-check-input me-1" type="checkbox" value="" aria-label="..."/>
+                        {vehicle.description}
+                    </li>
+                )}
+            </ul>
+        );
     }
 
-    handleClick = (e) => {
-        console.log(e.latlng)
+    static renderShipmentList(shipments) {
+        return (
+            <ul className="list-group">
+                <h5>Shipments</h5>
+                {shipments.map(vehicle =>
+                    <li className="list-group-item">
+                        <input className="form-check-input me-1" type="checkbox" value="" aria-label="..."/>
+                        {vehicle.description}
+                    </li>
+                )}
+            </ul>
+        );
     }
 
     render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : ListPage.renderContents(this.state.vehicles, this.state.shipments);
+            
         return (
             <Container>
-                <Row>
-                    <Col>
-                        <div className="list-group">
-                            <button href="#" className="list-group-item list-group-item-action active" aria-current="true">
-                                The current link item
-                            </button>
-                            <button href="#" className="list-group-item list-group-item-action">A second link item</button>
-                            <button href="#" className="list-group-item list-group-item-action">A third link item</button>
-                            <button href="#" className="list-group-item list-group-item-action">A fourth link item</button>
-                            <button href="#" className="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</button>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="list-group">
-                            <button href="#" className="list-group-item list-group-item-action active" aria-current="true">
-                                The current link item
-                            </button>
-                            <button href="#" className="list-group-item list-group-item-action">A second link item</button>
-                            <button href="#" className="list-group-item list-group-item-action">A third link item</button>
-                            <button href="#" className="list-group-item list-group-item-action">A fourth link item</button>
-                            <button href="#" className="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</button>
-                        </div>
-                    </Col>
-                    <Col xs={6}>
-                    <MapContainer center={[ 48, 11 ]} zoom={10} scrollWheelZoom={true} eventHandlers={{
-                        click: () => {
-                        console.log('map clicked')
-                        },
-                    }}>
-                            <TileLayer
-                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <LocationMarker effectOn={this}/>
-                        </MapContainer>
-                    </Col>
-                </Row>
+                    {contents}
             </Container>
         );
+    }
+
+    async populateShipmentData() {
+        const response = await fetch('Shipment/all');
+        const data = await response.json();
+        this.setState({ shipments: data});
+    }
+
+    async populateVehicleData() {
+        const response = await fetch('Vehicle/all');
+        const data = await response.json();
+        this.setState({ vehicles: data});
+    }
+
+    static renderContents(vehicles, shipments) {
+        let vehicleContent =  ListPage.renderVehicleList(vehicles);
+        let shipmentContent = ListPage.renderShipmentList(shipments);
+        
+        return (
+            <Row>
+                <Col>
+                    {vehicleContent}
+                </Col>
+                <Col>
+                    {shipmentContent}
+                    <div>
+                        <Button
+                            color="primary">
+                            Create route plan
+                        </Button>
+                    </div>
+                </Col>
+                <Col xs={6}>
+                    <MapContainer center={[48, 11]} zoom={10} scrollWheelZoom={true} eventHandlers={{
+                        click: () => {
+                            console.log('map clicked')
+                        },
+                    }}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <LocationMarker effectOn={this}/>
+                    </MapContainer>
+                </Col>
+            </Row>
+        );
+    }
+
+    populateLists() {
+        this.populateVehicleData();
+        this.populateShipmentData()
+        this.setState({ loading: false });
     }
 }
