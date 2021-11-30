691683b7cccd269a572a56d4a9e117173754c78b
Peter Langgaard Pedersen <pped@itu.dk>
2021-11-30 08:43:00 +0100

    Added some select input

--- ChaingeRoutePlanner/ChaingeRoutePlanner/ClientApp/src/components/OldRoutesPage.js
+++ ChaingeRoutePlanner/ChaingeRoutePlanner/ClientApp/src/components/OldRoutesPage.js
@@ -11,7 +11,8 @@ export class OldRoutesPage extends Component {
             selectedRoutes: [],
             selectedR: null,
             center: [55.66064229583371, 12.59125202894211],
-            selectedOption: null
+            selectedOption: 'All',
+            date: null
         };
 
         this.getData = this.getData.bind(this);
@@ -40,15 +41,15 @@ export class OldRoutesPage extends Component {
         return (
             <Container>
                 <Row>
-                    <h5>Old Routes</h5>'
+                    <h5>Old Routes</h5>
                 </Row>
                 <Row>
                     <Col>
-                        <Input type="date" name='date_of_delivery' onChange={event => this.state.data = event.target.value} />
+                        <Input type="date" name='date_of_delivery' onChange={event => this.setState({ date: event.target.value })} />
                     </Col>
                     <Col>
                         <Input type="select" name="select" id="exampleSelect" value={this.state.selectedOption} onChange={event => this.setState({ selectedOption: event.target.value})}>
-                            <option value={null}>All</option>
+                            <option value={'All'}>All</option>
                             <option value={0}>1</option>
                             <option value={1}>2</option>
                             <option value={2}>3</option>
@@ -58,8 +59,8 @@ export class OldRoutesPage extends Component {
                 <br/>
                 <ul className="list-group">
                 {routes.map((routes, index) =>
-                (index % 3 == this.state.selectedOption || null == this.state.selectedOption)  && <li className="list-group-item" key={index + 'routeID'}>
-                        <Input onClick={() => this.handleRouteSelect(routes.routes, index)} className="form-check-input me-1 changie_checkBox" type="button" value={'Bike ' + (1 + index % 3) + " / " + 'Bike ' + (1 + index % 3) } aria-label="..." style={{ width: '100%', height: '100%', backgroundColor: index == this.state.selectedR ? 'rgb(105, 219, 26)' : '', color: index == this.state.selectedR ? ' white' : '', fontWeight: 'bold'}} >
+                    (index % 3 == this.state.selectedOption || 'All' == this.state.selectedOption) && this.state.date != null && <li className="list-group-item" key={index + 'routeID'}>
+                        <Input onClick={() => this.handleRouteSelect(routes.routes, index)} className="form-check-input me-1 changie_checkBox" type="button" value={this.state.date + " / " + 'Bike ' + (1 + index % 3) } aria-label="..." style={{ width: '100%', height: '100%', backgroundColor: index == this.state.selectedR ? 'rgb(105, 219, 26)' : '', color: index == this.state.selectedR ? ' white' : '', fontWeight: 'bold'}} >
                         </Input>
                         <div style={{ textAlign: 'center' }}>
                             <label style={{ fontSize: '10px', color: Object.keys(this.state.routes).length / 2 > index ? 'black' : 'rgb(105, 219, 26)' }}>
